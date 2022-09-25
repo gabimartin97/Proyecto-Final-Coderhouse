09ed24d91eef0a1da58428ff9aa1c311b2da5c32
Gabriel Martin <gabriel97martin@gmail.com>
2022-09-25 16:44:25 -0300

    Importada escena MenuPrincipal y GameManager.

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,32 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    
+    private static GameManager instance;
+    private static int score;
+    private static bool isGameOver = false;
+    private static int difficultyLevel = 1;
+
+    public static int Score { get => score; set => score = value; }
+    public static bool IsGameOver { get => isGameOver; set => isGameOver = value; }
+    
+    public static GameManager Instance { get => instance; set => instance = value; }
+    public static int DifficultyLevel { get => difficultyLevel; set => difficultyLevel = value; }
+
+    private void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+        
+    }
+    
+    
+}
---
+++ Assets/Scripts/UI/MainMenu.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    [SerializeField] Slider spawnLevelSlider;
+     
+    public void OnClickPlay()
+    {
+       // SceneManager.LoadScene("EscenaDemoShooter");
+    }
+
+    public void OnClickSpawnLevel()
+    {
+       
+        GameManager.DifficultyLevel = (int)spawnLevelSlider.value;
+    }
+}
